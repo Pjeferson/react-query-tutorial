7e7984c8a94baaf1308aa25dedf4addd9e7021a8
Pjeferson <paulojeferson34@gmail.com>
2021-12-26 10:36:10 -0300

    Experimenting react-query-devtools

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { ReactQueryDevtools } from "react-query-devtools";
 import Navbar from "./components/Navbar";
 import Planets from "./components/Planets";
 import People from "./components/People";
@@ -7,15 +8,19 @@ function App() {
   const [page, setPage] = useState('planets');
 
   return (
-    <div className="App">
-      <h1>Star Wars Info</h1>
+    <>
+      <div className="App">
+        <h1>Star Wars Info</h1>
 
-      <Navbar setPage={setPage}/>
+        <Navbar setPage={setPage}/>
 
-      <div className="content">
-        { page === 'planets' ? <Planets /> : <People /> }
+        <div className="content">
+          { page === 'planets' ? <Planets /> : <People /> }
+        </div>
       </div>
-    </div>
+
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </>
   );
 }
 
